4289888b41b97f73c2acd74ff74eaa29f1b870e0
Afonso Dias <afonso-dias2011@hotmail.com>
2019-04-18 02:26:03 -0300

    feat: adding validation in pokemon model + adding rspec to project + testing pokemon model

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -6,4 +6,10 @@ class Pokemon
   field :type, type: Array
   field :sprite, type: String
   field :evolution_chain, type: Array
+
+  validates :pokemon_number, presence: true
+  validates :name, presence: true
+  validates :type, presence: true
+  validates :evolution_chain, presence: true
+
 end
---
+++ spec/models/pokemon_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Pokemon, type: :model do
+  context "Validate Pokemon" do
+    it "Pokemon is valid?" do
+      pokemon = Pokemon.new
+      pokemon.name = "chikorita"
+      pokemon.pokemon_number = 152
+      pokemon.type = ["grass"]
+      pokemon.evolution_chain = ["chikorita", "bayleef", "meganium"]
+
+      expect(pokemon.valid?).to be_truthy
+    end
+  end
+end
